118771b1a78b953af11d025c37b0bc5de75e5adf
Xtron64 <snekkieboi@pm.me>
2023-05-13 20:34:26 +0100

    Restructure project

--- flashcardRevision.py
+++
@@ -1,5 +0,0 @@
-import json
-if 'flashcard.json' != None:
-    print("You don't have any flashcards. Please complete the following questions in order to create one:")
-    import flashcardCreation
-json.loads('flashcard.json')
